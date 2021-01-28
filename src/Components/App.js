93121bcb3c4e47a8452f2ddefbe04f0a1d86bac5
Sean Kramer <seandominickramer@gmail.com>
2021-01-27 20:42:42 -0500

    Add SignUpForm

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,8 +1,14 @@
 import React from 'react'
+import LoginForm from './LoginForm'
+import SignUpForm from './SignUpForm'
 
 const App = () => {
   return (
+    <>
     <div>Welcome to Family Recipes App!</div>
+    <SignUpForm />
+    <LoginForm />
+    </>
   )
 }
 
---
+++ src/Components/LoginForm.js
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
+import axios from 'axios'
+
+const LoginForm = () => {
+
+  const [credentials, setCredentials] = useState({
+    username: '',
+    password: ''
+  })
+
+const onChangeHandler = (e) => {
+console.log(e.target.value);
+setCredentials({
+  ...credentials,
+  [e.target.name]: e.target.value
+})
+}
+
+const onSubmitHandler = (e) => {
+e.preventDefault()
+console.log('submitted!');
+// axios.post('', credentials)
+// .then((res) => {
+//   console.log(res);
+//   localStorage.setItem('token', res.data.payload)
+// })
+// .catch(err => console.log(err))
+}
+
+  return (
+    <Form onSubmit={onSubmitHandler}>
+          <FormGroup>
+        <Label for="username">Username</Label>
+        <Input type="text" name="username" id="username" placeholder="enter your username" onChange={onChangeHandler}/>
+      </FormGroup>
+      <FormGroup>
+        <Label for="examplePassword">Password</Label>
+        <Input type="password" name="password" id="examplePassword" placeholder="enter your password" onChange={onChangeHandler}/>
+      </FormGroup>
+      <Button>Submit</Button>
+    </Form>
+  )
+}
+
+export default LoginForm
