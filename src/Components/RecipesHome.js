2c4d36d2b8f6c2757a194aeb7b32319d4af73fdc
Sean Kramer <seandominickramer@gmail.com>
2021-01-28 20:38:38 -0500

    Add RecipeCard and change build script to CI= react-scripts build

---
+++ src/Components/RecipesHome.js
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from 'react';
+import RecipeCard from './RecipeCard'
+import axiosWithAuth from './axiosWithAuth'
+
+const RecipesHome = () => {
+
+const [recipeNames, setRecipeNames] = useState([])
+
+const getRecipes = () => {
+axiosWithAuth()
+.get("")
+.then((res) => console.log('get data: ', res.data))
+.catch(err => console.log(err))
+}
+
+useEffect(() => {
+getRecipes()
+}, [])
+
+  return (
+    <div>
+      <h2>Your Recipes!</h2>
+      {recipeNames.map((recipeName)=> 
+      <RecipeCard recipeName={recipeName} />
+      )}
+      
+    </div>
+  );
+};
+
+export default RecipesHome;
