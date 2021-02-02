d82002a04e64101b4383f8c9e751d9f8a4f9c878
Sean Kramer <seandominickramer@gmail.com>
2021-02-02 16:14:46 -0500

    Add functioning EditRecipeForm

--- src/Components/RecipeCard.js
+++ src/Components/RecipeCard.js
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Card, Button, CardTitle, CardText } from 'reactstrap';
 import { Link } from 'react-router-dom'
 import { axiosWithAuth } from './axiosWithAuth';
 import axios from 'axios'
+import EditRecipeForm from './EditRecipeForm';
 
 
 const RecipeCard = (props) => {
 console.log('card props: ', props);
+
+const [clicked, setClicked] = useState(false)
 
 const id = props.recipeName.id
 console.log('id:', id);
@@ -31,11 +34,13 @@ const editRecipe = () => {
     <div>
       <Card body outline color='success' style={{backgroundColor: '#E6BD8F'}}>
         <CardTitle tag='h5'>{props.recipeName.name}</CardTitle>
-       <Link key={id} to={`/Home/instructions/${id}`}>
-       <h5>See instructions</h5>
+       <Link key={id} to={`/Home/instructions/${id}`} style={{color: '#347382'}}>
+       <h5>Click for instructions</h5>
        </Link>
-       <Button onClick={deleteRecipe}>Delete Recipe</Button>
-       <Button>Edit Recipe</Button>
+       <Button style={{marginTop: '10px', width: '170px', backgroundColor: '#DE7F6E'}} onClick={deleteRecipe}>Delete Recipe</Button>
+       
+       <Button style={{marginTop: '10px', width: '170px', backgroundColor: '#93A586'}} onClick={() => {setClicked(!clicked)}}>Edit Recipe</Button>
+{clicked && <EditRecipeForm id={props.recipeName.id}/>}
       </Card>
     </div>
   );
